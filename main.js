5c555fd1ed6e80f06faf2a1d0dba7c51e0d8b4d7
Guiming <781517026@qq.com>
2023-02-20 21:04:15 +0800

    完成home的开发

--- main.js
+++ main.js
@@ -1,5 +1,40 @@
 import App from './App'
 
+// 配置网络请求
+import {$http} from '@escook/request-miniprogram'
+uni.$http = $http
+// 配置请求根路径
+$http.baseUrl = 'https://www.uinav.com'
+// 请求开始之前，做一些事情
+$http.beforeRequest = () => {
+	uni.showLoading({
+		title: '数据加载中...'
+	})
+}
+// 请求完成之后，做一些事情
+$http.afterRequest = (res) => {
+	uni.hideLoading()
+	if(res.data.meta.status !== 200) {
+		return uni.showToast({
+			title: '数据请求失败！',
+			duration: 1500,
+			icon: 'none'
+		})
+	}
+}
+
+// 封装展示消息提示的方法
+uni.$showMsg = function(title = '数据请求失败！', duration = 1500) {
+	uni.showToast({
+		title,
+		duration,
+		icon: 'none'
+	})
+}
+
+
+
+
 // #ifndef VUE3
 import Vue from 'vue'
 Vue.config.productionTip = false
